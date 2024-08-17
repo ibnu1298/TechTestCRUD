96e4373debcba39aefeba4a2d3e7cf8dd9bc13a8
Ibnu Aqil Al Barohin <kalangkang1298@gmail.com>
2024-08-17 08:25:32 +0700

    Add project files.

---
+++ TechTestCRUD/EmployeeServices.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTestCRUD
+{
+
+    public interface IEmployee
+    {
+        Employee Create(string fullName, DateTime birthDate);
+        IEnumerable<Employee> ReadAll();
+        Employee ReadById(string id);
+        Employee Update(string id, string fullName, DateTime birthDate);
+        bool Delete(string id);
+    }
+  
+    public class EmployeeServices : IEmployee
+    {
+        private readonly List<Employee> _employees;
+        private int _nextId;
+
+        public EmployeeServices()
+        {
+            _employees = new List<Employee>();
+            _nextId = 1001;
+        }
+
+        public Employee Create(string fullName, DateTime birthDate)
+        {
+            try
+            {
+                foreach (var item in _employees)
+                {
+                    if (item.FullName == fullName)
+                    {
+                        throw new Exception($"Karyawan dengan Nama {fullName} Sudah Terdaftar\n");
+                    }
+                }
+                var employee = new Employee
+                {
+                    EmployeeId = (_nextId++).ToString(),
+                    FullName = fullName,
+                    BirthDate = birthDate
+                };
+                _employees.Add(employee);
+                return employee;
+               
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public IEnumerable<Employee> ReadAll()
+        {
+            try
+            {
+                if (_employees.Count == 0)
+                {
+                    throw new Exception("Belum Ada Data\n\n");                    
+                }
+                return _employees;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Employee ReadById(string id)
+        {
+            try
+            {
+                var employee = _employees.SingleOrDefault(e => e.EmployeeId == id);
+                return employee ?? throw new Exception($"Karyawan dengan Employee ID : {id} Tidak ditemukan\n");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Employee Update(string id, string fullName, DateTime birthDate)
+        {
+            try
+            {
+                var employee = _employees.SingleOrDefault(e => e.EmployeeId == id);
+                if (employee != null)
+                {
+                    employee.FullName = fullName;
+                    employee.BirthDate = birthDate;
+                }
+                else
+                {
+                    throw new Exception($"Karyawan dengan Employee ID : {id} Tidak ditemukan\n");
+                }
+                return employee;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool Delete(string id)
+        {
+            try
+            {
+                var employee = _employees.SingleOrDefault(e => e.EmployeeId == id);
+                if (employee != null)
+                {
+                    _employees.Remove(employee);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ TechTestCRUD/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TechTestCRUD;
+
+class Program
+{
+    static void Main()
+    {
+        IEmployee service = new EmployeeServices();
+        Console.WriteLine();
+        //Create
+        var employee1 = service.Create("Adit", new DateTime(1954, 8, 17));
+        var employee2 = service.Create("Anton", new DateTime(1954, 8, 18));
+        var employee3 = service.Create("Amir", new DateTime(1954, 8, 19));
+        Console.WriteLine();
+        // Read
+        Console.WriteLine("Daftar Karyawan");
+        foreach (var employee in service.ReadAll())
+        {
+            Console.WriteLine($"{employee.EmployeeId}: {employee.FullName}, {employee.BirthDate.ToShortDateString()}");
+        }
+        Console.WriteLine();
+        // Update
+        var updatedEmployee = service.Update(employee1.EmployeeId, "Adit Update", new DateTime(2014, 5, 20));
+        if (updatedEmployee != null)
+        {
+            Console.WriteLine($"Updated Data Karyawan\n{updatedEmployee.EmployeeId}: {updatedEmployee.FullName}, {updatedEmployee.BirthDate.ToShortDateString()}");
+        }
+        Console.WriteLine();
+        // Delete
+        bool deleted = service.Delete(employee2.EmployeeId);
+        if (deleted)
+        {
+            Console.WriteLine($"Hapus data Karyawan dengan ID : {employee2.EmployeeId}");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"Setelah Delete Data Karyawan");
+        foreach (var employee in service.ReadAll())
+        {
+            Console.WriteLine($"{employee.EmployeeId}: {employee.FullName}, {employee.BirthDate.ToShortDateString()}");
+        }
+        Console.WriteLine();
+        var employee4 = service.Create("Budi Baru", new DateTime(1999, 8, 20));
+        var employee5 = service.Create("Ani Baru", new DateTime(2000, 8, 20));
+
+        Console.WriteLine("Setelah ditambah 2 data Baru");
+        Console.WriteLine(new string('-', 35)); // Garis pemisah
+        Console.WriteLine($"{"ID",-5} {"| FullName",-12} {" | Birth Date",-12}");
+        Console.WriteLine(new string('-', 35)); // Garis pemisah
+        foreach (var employee in service.ReadAll())
+        {
+            Console.WriteLine($"{employee.EmployeeId,-5} | {employee.FullName,-12}| {employee.BirthDate:dd-MMM-yy}");
+        }
+    }
+}
